b12a24680472d68a7f7e63ab05adfc26ccf15e53
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-06-28 13:06:17 +0300

    new e-commerce

--- src/App.js
+++ src/App.js
@@ -1,102 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { Switch, Route,BrowserRouter as Router } from "react-router-dom";
-import "./App.css"
-import NavBar from "./component/NavBar/NavBar";
-import axios from "axios";
-import Home from "./Pages/Home";
-import Basket from "./component/Basket/Basket";
-import Detail from "./Pages/Detail";
-import Favories from "./component/Favories/Favories";
-
-const API_URL = "https://60b8a865b54b0a0017c04257.mockapi.io/api/v1/products";
-function App() {
-  const [products, setProducts] = useState([]);
-  const [basket, setBasket] = useState([]);
-  const [favori, setFavori] = useState([]);
-  useEffect(() => {
-    axios.get(API_URL).then((response) => setProducts(response.data));
-  }, []);
-
-  const addBasket = (product) => {
-    const productArr = basket.find((item) => item.id === product.id);
-    if (productArr) {
-      setBasket(
-        basket.map((item) =>
-          item.id === product.id
-            ? { ...productArr, count: productArr.count + 1 }
-            : item
-        )
-      );
-    } else {
-      setBasket([...basket, { ...product, count: 1 }]);
-      console.log("eklendi",setBasket)
-    }
-  };
-  const addFavories = (product) => {
-    const productArr = favori.find((item) => item.id === product.id);
-    if (!productArr) {
-      setFavori([...favori, { ...product, favorite: true }]);
-    }
-
-    alert("Favorilere Eklendi!");
-    console.log("eklendi",setFavori)
-  };
-  const removeBasket = (product) => {
-    const productArr = basket.find((item) => item.id === product.id);
-    if (productArr) {
-      if (productArr.counter === 0) {
-        productArr.counter -= 1;
-        setBasket([
-          ...basket.filter((item) => item.id !== product.id),
-          productArr,
-        ]);
-      } else {
-        setBasket([...basket.filter((item) => item.id !== product.id)]);
-        console.log("Silindi")
-      }
-    }
-  };
-  const removeFavorite = (product) => {
-    const productArr = favori.find((item) => item.id === product.id);
-    if (productArr) {
-      setFavori([...favori.filter((item) => item.id !== product.id)]);
-    }
-  };
-  useEffect(() => {
-    console.log(favori);
-  }, [favori]);
-
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import SignIn from "./Pages/Auth/SignIn/SignIn";
+import SignUp from "./Pages/Auth/Signup/SignUp";
+export default function App() {
   return (
-    <div className="App">
-      <Router>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <div id="#content">
+
 
         <Switch>
-          <Route exact path="/">
-            <NavBar />
-            {/* <Basket basket={basket} /> */}
-            <Home
-              products={products}
-              basket={basket}
-              addProduct={addBasket}
-              addFavori={addFavories}
-              onRemoveFavori={removeFavorite}
-              removeProductBasket={removeBasket}
-              />
-          </Route>
-          <Route path="/favori">
-            <NavBar />
-            <Basket basket={basket} />
-            <Favories favorities={favori} addFavories={removeFavorite} />
-            </Route>
-            <Route path="/">
-            <NavBar />
-            <Basket basket={basket} />
-            <Detail products={products} />
-          </Route>
+          <Route path="/" exact component={Home} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+
         </Switch>
-          </Router>
-    </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react"
+import "./reset.css"
+import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
+    <ChakraProvider>
+
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
